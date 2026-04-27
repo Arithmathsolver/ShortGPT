49e12061c55292f8e9c7603df364a92aee9535eb
Arithmathsolver <daniellemayian22@gmail.com>
2026-04-27 04:42:32 -0700

    Expose upload_to_youtube in api_utils package
    Added explicit exposure of upload_to_youtube from image_api.

--- shortGPT/api_utils/__init__.py
+++ shortGPT/api_utils/__init__.py
@@ -1,2 +1,8 @@
+# shortGPT/api_utils/__init__.py
+
+# Import modules
 from . import image_api
 from . import eleven_api
+
+# Explicitly expose upload_to_youtube at package level
+from .image_api import upload_to_youtube
